71ac11218179148f2435a3979a8baaf05af507f9
larchuto <johan.lafon@phelma.grenoble-inp.fr>
2014-04-14 08:24:39 +0200

    add object 2

---
+++ obstacle.cpp
@@ -0,0 +1,33 @@
+#include "obstacle.h"
+
+obstacle::obstacle(int xpos, int ypos) : m_x_pos(xpos), m_y_pos(ypos) //, m_img(imgPath)
+{
+    //ctor
+}
+
+obstacle::~obstacle()
+{
+    //dtor
+}
+
+/*obstacle::obstacle(const obstacle& other)
+{
+    //copy ctor
+}*/
+
+int obstacle::get_x_pos() const
+{
+    return m_x_pos;
+}
+int obstacle::get_y_pos() const
+{
+    return m_y_pos;
+}
+void obstacle::chg_x_pos(int nXpos)
+{
+    m_x_pos=nXpos;
+}
+void obstacle::chg_y_pos(int nYpos)
+{
+    m_y_pos=nYpos;
+}
